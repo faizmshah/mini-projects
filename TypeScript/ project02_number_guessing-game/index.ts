e45e43ade8083daa1c91d5609106f5bfe97df76e
Faiz Muhammad Shah <tofeequeshah456@gmail.com>
2024-05-04 02:17:02 +0500

    Add files via upload

---
+++ TypeScript/ project02_number_guessing-game/index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 6 + 1); // computer will generate a randum number.
+
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1-6:",
+    },
+]);
+
+if(answers.userGuessedNumber === randomNumber) {
+    console.log("Congratulations! You guessed right number.")
+}else{
+    console.log("You guessed a wrong number. Try again!")
+}
